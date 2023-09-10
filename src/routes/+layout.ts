7af2cca57235b80cf2c526c3ecfb27a9f1a84b8b
eddie <kyggiik4689@gmail.com>
2023-09-10 23:33:00 +0800

    trailingSlash always

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,4 +1,4 @@
 export const prerender = true;
 export const ssr = false;
-export const trailingSlash = 'never';
-// export const trailingSlash = 'always'
+// export const trailingSlash = 'never';
+export const trailingSlash = 'always'
