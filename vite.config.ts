99bee28b7e0ddccef58472240cac4d341f75f657
EddieLiu <eddieliu58@gmail.com>
2023-09-16 15:12:08 +0800

    build:prod

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
+const mode = process.env.APP_ENV // This now exists.
 
 export default defineConfig({
+	mode: mode, // This will set the mode, to avoid confusions.
 	plugins: [sveltekit()],
 	server: {
-	  proxy: {
+		proxy: {
 			'/api': {
 				target: 'https://api.inovels.click',
 				rewrite: (path) => path.replace(/^\/api/, ''),
@@ -15,7 +17,7 @@ export default defineConfig({
 			rewrite: (path) => path.replace(/^\/api/, ''),
 			changeOrigin: true
 		},
-	  }
+		}
 	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
