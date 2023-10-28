c4fcdd36c010bea3a65b0e55cb8d79d625d66fa6
EddieLiu <eddieliu58@gmail.com>
2023-10-28 19:22:25 +0800

    ip

---
+++ apps/search-ip/src/routes/+page.server.ts
@@ -0,0 +1,28 @@
+import type { MetaTagsProps } from 'svelte-meta-tags'; // Import type for meta tags properties.
+import { requestIp } from "../hooks.server";
+/** @type {import('@sveltejs/kit').RequestHandler} */
+export const load = async ({ url }) => {
+  // Define meta tags for this specific child page.
+  const metaTags: MetaTagsProps = Object.freeze({
+    title: 'ip搜尋', // Page-specific title.
+    description: '', // This description will override the default.
+    openGraph: {
+      // OpenGraph meta tags specific to this page.
+      type: 'website',
+      url: new URL(url.pathname, url.origin).href,
+      title: 'ip搜尋 - 自由創作者股份有限公司',
+      description: '',
+      images: [ {
+        url: `${url}images/logo.webp`,
+        width: 800,
+        height: 600,
+        alt: 'ip搜尋 logo alt'
+      },]
+    }
+  });
+
+  return {
+    metaTagsChild: metaTags, // Return meta tags so they can be consumed by layout.svelte.
+    requestIp
+  };
+};
---
+++ apps/search-ip/src/routes/+page.svelte
@@ -0,0 +1,92 @@
+<script lang="ts">
+	import '@splidejs/splide/dist/css/themes/splide-default.min.css';
+	import axios from 'axios';
+	import { PUBLIC_PROD_BASE_URL } from '$env/static/public';
+	import Geolocation from 'svelte-geolocation';
+	/** @type {import('./$types').PageData} */
+	export let data;
+	const baseUrl = PUBLIC_PROD_BASE_URL;
+
+	let IPV4List = {};
+	let IPV6List = {};
+	let currentIP = data.requestIp;
+	let coords: Array<String> = [];
+	async function fetchList() {
+		let getIPV4 = await axios.get(`${baseUrl}/itools/ip-range/v4?ip=${currentIP}&uid=test`);
+		IPV4List = { ...getIPV4.data };
+		let getIPV6 = await axios.get(`${baseUrl}/itools/ip-range/v6?ip=${currentIP}&uid=test`);
+		IPV6List = { ...getIPV6.data };
+	}
+</script>
+
+<Geolocation getPosition bind:coords />
+<section
+	class="mx-auto flex min-h-[600px] max-w-full items-center justify-center px-6 md:max-w-6xl"
+>
+	<div class="mx-auto grid w-full grid-cols-1 gap-4 py-12 md:max-w-5xl md:grid-cols-2">
+		<div class="">
+			<h1 class="text-6xl font-bold">IP搜尋</h1>
+			<div class="mt-10">
+				<div class="">
+					<label for="1" class=" text-sm text-gray-600">請輸入您想知道的 IP 位址：</label>
+				</div>
+				<div class="flex items-center">
+					<div class="">
+						<input
+							type="text"
+							class="box-border rounded-md border-[2px] border-solid border-gray-400 p-1 outline-none transition-shadow focus:border-blue-400"
+							id="1"
+							bind:value={currentIP}
+						/>
+					</div>
+					<div class="ml-4">
+						<button
+							class="rounded-full bg-orange-300 p-4 text-center text-white"
+							type="button"
+							on:click={fetchList}>取得 IP 詳細資料</button
+						>
+					</div>
+				</div>
+				<div class="mt-4">
+					<p class=" text-xs text-gray-600">
+						此網站所使用的 IP2Location LITE 數據來自於 <a href="https://lite.ip2location.com"
+							>https://lite.ip2location.com</a
+						>.
+					</p>
+				</div>
+			</div>
+		</div>
+		<div class=" min-h-[500px] bg-[#282c34] px-8 py-4 text-[#ABB2BF]">
+			{#if Object.keys(IPV4List).length > 0}
+				<div>
+					<h2 class="text-base font-bold">IPv4</h2>
+					<ul class="mt-2">
+						{#each Object.entries(IPV4List) as [key, value]}
+							{#if key !== 'success' && key !== 'msg'}
+								<li class="mb-4">{key} : <span class="text-[#98C379]">{value}</span></li>
+							{/if}
+						{/each}
+					</ul>
+				</div>
+			{/if}
+			{#if Object.keys(IPV6List).length > 0}
+				<div class="mt-2">
+					<h2 class="text-base font-bold">IPv6</h2>
+					<ul class="mt-2">
+						{#each Object.entries(IPV6List) as [key, value]}
+							{#if key !== 'success' && key !== 'msg'}
+								<li class="mb-4">{key} : <span class="text-[#98C379]">{value}</span></li>
+							{/if}
+						{/each}
+					</ul>
+				</div>
+			{/if}
+		</div>
+	</div>
+</section>
+
+<style lang="postcss">
+	/* :global(html) {
+		background-color: theme(colors.gray.100);
+	} */
+</style>
