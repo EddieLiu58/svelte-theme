070d276d717c745430ea49cc1b0e079b9478869b
EddieLiu <eddieliu58@gmail.com>
2023-11-25 21:41:31 +0800

    tailwindcss default css

--- apps/itool/tailwind.config.js
+++ apps/itool/tailwind.config.js
@@ -1,32 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  // important: '#app',
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  theme: {
-    extend: {
-      colors: {
-        transparent: 'transparent',
-        footer: '#2d3748',
-        search: '#f7fafc',
-        bg: '#f2f3f4',
-      },
-    },
-    screens: {
-      sm: '640px',
-      // => @media (min-width: 640px) { ... }
+	// important: '#app',
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				transparent: 'transparent',
+				footer: '#2d3748',
+				search: '#f7fafc',
+				text: '#26282d',
+				bg: '#f2f3f4'
+			}
+		},
+		screens: {
+			sm: '640px',
+			// => @media (min-width: 640px) { ... }
 
-      md: '768px',
-      // => @media (min-width: 768px) { ... }
+			md: '768px',
+			// => @media (min-width: 768px) { ... }
 
-      lg: '1024px',
-      // => @media (min-width: 1024px) { ... }
+			lg: '1024px',
+			// => @media (min-width: 1024px) { ... }
 
-      xl: '1280px',
-      // => @media (min-width: 1280px) { ... }
+			xl: '1280px',
+			// => @media (min-width: 1280px) { ... }
 
-      '2xl': '1536px',
-      // => @media (min-width: 1536px) { ... }
-    },
-  },
-  plugins: [],
-};
+			'2xl': '1536px'
+			// => @media (min-width: 1536px) { ... }
+		}
+	},
+	plugins: []
+}
